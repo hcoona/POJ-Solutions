10dc89bb9c0ac9314c492002b66393eeed10ae09
Zhang Shuai <satan.student@gmail.com>
2013-06-18 22:05:52 +0800

    Fix problem Id.

---
+++ P1000.cpp
@@ -0,0 +1,17 @@
+#include "EntryPoint.h"
+
+namespace POJ {
+	class P1001 : public SolutionBase {
+	public:
+		virtual void Run(istream &in, ostream &out);
+	};
+
+	void P1001::Run(istream &in, ostream &out)
+	{
+		int a, b;
+		in >> a >> b;
+		out << a + b << endl;
+	}
+}
+
+ENTRY_POINT(POJ::P1001)
